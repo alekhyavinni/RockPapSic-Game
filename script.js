325a9cedee9aed05c1ba3d93d2ff9f3cbc864349
bootcamp vinni <alekhyavinni12@gmail.com>
2023-05-03 18:59:41 -0400

    programmed code for game

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-function 
+var RPSgame ={
+    Playoptions:['R','P','S'],
+    wins : 0,
+    loses :0 ,
+    ties : 0,
+    UserInp :'',
+    computerInp:'',
+
+    Prompt:function(){
+        var userchoice = window.prompt('Enter R/S/P');
+
+        if(!userchoice){
+            return;
+        }
+
+        this.UserInp=userchoice.toUpperCase();
+        console.log("user input",this.UserInp);
+        window.alert('user chose : '+this.UserInp);
+        this.generatecompinput();
+    },
+
+    generatecompinput:function(){
+         var randominp=Math.floor(Math.random()*this.Playoptions.length);
+         this.computerInp=this.Playoptions[randominp];
+         console.log("computer choice",this.computerInp);
+         window.alert('computer chose : '+this.computerInp);
+         this.results();
+    },
+
+    results:function(){
+      if(this.UserInp === this.computerInp){
+        this.ties++;
+        window.alert("Its a Tie");
+      }
+       else if((this.UserInp==='R' && this.computerInp==='P') ||
+          (this.UserInp==='S' && this.computerInp==='R') || 
+          (this.UserInp==='P' && this.computerInp==='S')){
+
+            this.loses++;
+            window.alert("computer wins");
+          }  
+        else{
+            this.wins++;
+        window.alert("U won");
+        }
+
+     window.alert('wins:'+this.wins +
+                   '\n loses:'+this.loses +
+                   '\n ties:' +this.ties);
+                   this.replay();
+    },
+
+    replay:function(){
+var playagain =window.confirm('wanna play again?');
+if(playagain){
+    this.play();
+}
+    },
+
+    play:function(){
+        this.Prompt();
+    },
+
+};
+
+
+
